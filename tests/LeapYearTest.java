ae9d0fc76ec3795dcdfd11f62c6f60d67494caad
ramachandracse2008 <ramachandracse2008@gmail.com>
2026-08-18 05:36:31 +0530

    Create LeapYearTest for leap year validation
    Added unit tests for leap year functionality.

---
+++ tests/LeapYearTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LeapYearTest {
+    @Test
+    public void testLeapYears() {
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(2024));
+    }
+
+    @Test
+    public void testNonLeapYears() {
+        assertFalse(LeapYear.isLeapYear(1900));
+        assertFalse(LeapYear.isLeapYear(2023));
+    }
+}
