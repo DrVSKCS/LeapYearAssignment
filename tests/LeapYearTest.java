ee83b5db8a0791f7876f4708a5f276194c42e54f
ramachandracse2008 <ramachandracse2008@gmail.com>
2026-08-18 06:41:16 +0530

    Update LeapYearTest.java

--- tests/LeapYearTest.java
+++ tests/LeapYearTest.java
@@ -1,5 +1,16 @@
+package tests;
 import static org.junit.Assert.*;
 import org.junit.Test;
+
+
+
+public class LeapYearTest {
+    @Test
+    public void testLeapYears() {
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(2024));
+    }
+}
 
 public class LeapYearTest {
     @Test
